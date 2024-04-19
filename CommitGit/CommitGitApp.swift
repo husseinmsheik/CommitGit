7f7160c55e47a72f5de71e1fd03e7feea69e870a
husseinmsheik <husseinmsheik199@gmail.com>
2024-04-19 11:58:11 +0300

    Initial Commit

---
+++ CommitGit/CommitGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitGitApp.swift
+//  CommitGit
+//
+//  Created by Hussein Msheik on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CommitGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
