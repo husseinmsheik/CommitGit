8946400a282eaa54af9a7c808f5d8051dd290450
husseinmsheik <husseinmsheik199@gmail.com>
2024-04-19 12:10:15 +0300

    comment text new

--- CommitGit/ContentView.swift
+++ CommitGit/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("This is a test")
+            Text("rwwrwrwrwrwr")
+            Text("fsffdgdg")
                 .padding()
         }
         .padding()
