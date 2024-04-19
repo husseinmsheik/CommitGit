b3190109baac1a197169563a9bac6b3f5ad592d2
husseinmsheik <husseinmsheik199@gmail.com>
2024-04-19 12:14:31 +0300

    create second view

---
+++ CommitGit/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  CommitGit
+//
+//  Created by Hussein Msheik on 19/04/2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
